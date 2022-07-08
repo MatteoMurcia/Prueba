63b21adacfae4784ec2d8eea7be694b6cdc5c448
Matteo Murcia <matteo@cbloom.com>
2022-07-07 23:00:27 -0500

    front.ejs

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
 app.get('/', (req,res) =>{
-    res.send("Welcome");
+    res.render('../views/front')
 });
 
 app.listen(PORT, function () { console.log(`Servidor corriendo en el puerto ${PORT}`) });
